88d0971765f077a06d5795c8e46b50566702a310
Estelaromero1314 <157331722+Estelaromero1314@users.noreply.github.com>
2024-01-21 21:31:32 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 // add your P5.js code here on the next line
-function setup(){
+function setup(){function setup(){
+  fill(0);
+  ellipse(50, 50, 50, 50)
+}
   fill(0);
   ellipse(50, 50, 50, 50)
 }
